54147eddececbe846d416880ad62569dc2525852
Kousik Saikia <kousik.saikia@gmail.com>
2022-11-06 16:50:53 +0530

    commit to develop

---
+++ Python_input_output.py
@@ -0,0 +1,11 @@
+int_var=10
+float_var=18.25
+string_var='Kousik Saikia'
+bool_var=True
+print('hello world')
+
+
+name = input("Enter your name : ")
+age = int(input("Enter age : "))
+print(name)
+print(age)
